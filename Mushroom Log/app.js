14812dc24d625a6580c092f271d882e7c51e4c8d
Carson Hunter <carsonh49er@gmail.com>
2023-05-14 22:03:34 -0700

    Add files via upload

---
+++ Mushroom Log/app.js
@@ -0,0 +1,175 @@
+document.querySelector("h1").onclick = () => {
+    alert(`
+    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡤⣤⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣸⡃⠀⠿⠇⢻⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡞⠁⠀⣰⡄⠀⠀⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⡇⠀⠸⡷⠃⢀⠀⠸⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⠋⢀⣀⡀⠀⢿⣳⡄⢳⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⠞⠁⠀⠸⣌⣻⠀⠀⠉⠁⠀⠙⢦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+    ⠀⠀⠀⠀⠀⠀⢀⡤⢾⢁⡞⠉⠉⠓⢦⡁⠀⠀⠀⠀⠀⠀⠀⠙⠲⣄⠀⠀⠀⠀⠀⠀⠀⠀
+    ⠀⠀⠀⠀⣠⡴⠯⠴⠋⠈⠧⣄⣀⣀⣠⠇⠀⠀⠶⠆⠀⢲⡆⠀⠀⠨⠷⠲⡄⠀⠀⠀⠀⠀
+    ⠀⠀⢀⣾⣅⢀⣠⠤⠶⠖⠒⠚⠛⠛⣟⠛⠛⠛⠒⡒⠒⠒⠷⠦⢤⣀⠀⠀⢻⡄⠀⠀⠀⠀
+    ⠀⠀⣼⢡⢟⣉⣀⠒⠶⠤⣄⡀⠀⠀⠈⢧⠀⠀⠀⡇⠀⠀⠀⡇⠀⠈⠑⣆⠀⢷⠀⠀⠀⠀
+    ⠀⠀⣿⡏⠀⢈⣉⣉⣉⣛⣒⣿⣿⣶⣤⠼⠗⠶⠾⣅⣀⡤⠞⣁⣤⠴⠂⠘⡇⢸⠀⠀⠀⠀
+    ⠀⠀⠘⣿⣄⠈⠀⠀⠀⢀⣠⣭⠟⠁⠀⠀⠀⠀⠀⠀⠙⢶⣋⠉⠀⠀⠀⢀⡇⡼⠀⠀⠀⠀
+    ⠀⠀⠀⠈⠛⠿⣶⣶⣿⣭⣥⡏⢀⣈⣈⠉⠀⠀⠐⠂⠠⠈⣏⣉⣉⣻⣶⣞⡵⠃⠀⠀⠀⠀
+    ⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⢹⠃⠻⢿⣿⠟⠀⠘⢾⣿⣿⠀⡿⠛⠛⠓⠋⠁⠀⠀⠀⠀⠀⠀
+    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⠀⠀⠀⠀⠀⠀⡄⠀⠉⠀⢰⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⡇⠀⠀⠀⣴⠀⠀⣹⡄⠀⠀⢸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠁⠀⠀⠀⠈⠈⠁⠉⠀⠀⠀⢸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⠀⠠⠤⠤⠤⠤⠤⠤⠄⠀⠘⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢧⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+    ⠀⠀⠀⠀⠀⠀⠀⠀⢰⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠳⣄⡀⠀⠀⠀⠀⠀⠀
+    ⠀⠀⠀⠀⠀⠀⠀⣠⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢦⡀⠀⠀⠀⠀
+    ⠀⠀⢀⣀⣀⣠⠞⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠳⣄⡀⠀⠀`)
+};
+const container = document.querySelector('#container');
+const description = document.querySelector('#description')
+const btn1 = document.querySelector('.button1');
+const btn2 = document.querySelector('.button2');
+const btn3 = document.querySelector('.button3');
+const btn4 = document.querySelector('.button4');
+const btn5 = document.querySelector('.button5');
+const btn6 = document.querySelector('.button6')
+const fungi1 = document.createElement('img');
+fungi1.src = "fungi1.jpg";
+const fungi2 = document.createElement('img');
+fungi2.src = "fungi2.jpg";
+const fungi3 = document.createElement('img');
+fungi3.src = "fungi3.jpg";
+const fungi4 = document.createElement('img');
+fungi4.src = "fungi4.jpg";
+const fungi5 = document.createElement('img');
+fungi5.src = "fungi5.jpg";
+const fungi6 = document.createElement('img');
+fungi6.src = "fungi6.jpg";
+const fungi7 = document.createElement('img');
+fungi7.src = "fungi7.png";
+const fungi8 = document.createElement('img');
+fungi8.src = "fungi8.png";
+const fungi9 = document.createElement('img');
+fungi9.src = "fungi9.png";
+const fungi10 = document.createElement('img');
+fungi10.src = "fungi10.png";
+
+function lightSpored() {
+    let hasBeenCalled = false;
+    btn1.addEventListener('click', () => {
+        if (!hasBeenCalled) {
+            hasBeenCalled = true;
+            container.append(fungi4);
+            container.append(fungi10)
+        } else {
+            hasBeenCalled = false;
+            container.innerHTML = ' ';
+        }
+    })
+};
+
+function brownSpored() {
+    let hasBeenCalled = false;
+    btn2.addEventListener('click', () => {
+        if (!hasBeenCalled) {
+            hasBeenCalled = true;
+            container.append(fungi3);
+        } else {
+            hasBeenCalled = false;
+            container.innerHTML = ' ';
+        }
+    })
+};
+
+function darkSpored() {
+    let hasBeenCalled = false;
+    btn3.addEventListener('click', () => {
+        if (!hasBeenCalled) {
+            hasBeenCalled = true;
+            container.append(fungi2);
+            container.append(fungi8);
+        } else {
+            hasBeenCalled = false;
+            container.innerHTML = ' ';
+        }
+    })
+};
+
+function crust() {
+    let hasBeenCalled = false;
+    btn4.addEventListener('click', () => {
+        if (!hasBeenCalled) {
+            hasBeenCalled = true;
+            container.append(fungi1);
+            container.append(fungi7);
+        } else {
+            hasBeenCalled = false;
+            container.innerHTML = ' ';
+        }
+    })
+};
+
+function puffed() {
+    let hasBeenCalled = false;
+    btn5.addEventListener('click', () => {
+        if (!hasBeenCalled) {
+            hasBeenCalled = true;
+            container.append(fungi6);
+        } else {
+            hasBeenCalled = false;
+            container.innerHTML = ' ';
+        }
+    })
+};
+
+function jelly() {
+    let hasBeenCalled = false;
+    btn6.addEventListener('click', () => {
+        if (!hasBeenCalled) {
+            hasBeenCalled = true;
+            container.append(fungi5);
+            container.append(fungi9);
+        } else {
+            hasBeenCalled = false;
+            container.innerHTML = ' ';
+        }
+    })
+};
+
+lightSpored();
+brownSpored();
+darkSpored();
+crust();
+puffed();
+jelly();
+
+// function info() {
+//     fungi1.addEventListener('click', () => {
+//         if (fungi1.click === true) {
+//             console.log('test')
+//         }
+//     })
+// }
+// info();
+
+
+//fungi1 = btn4 (polypores and crust)
+//fungi2 = btn3 (dark-spored)
+//fungi3 (Pholiota aurivella) = btn2 (brown-spored)
+//fungi4 (Winter Russula) = btn1 (light-spored)
+//fungi5 (Witches' Butter) = btn6 (jelly)
+//fungi6 (??? Undefined Puff) = btn5
+//fungi7 (Violet Crust) = btn4 (crust)
+//fungi8 (connifer tuft) = btn3 (dark-spored)
+//fungi9 (Black Witches' Butter) = btn6 (jelly)
+//fungi10 (Golden Waxcap) = btn1 (light-spored)
+
+//DESCRIPTIONS
+//fungi1, Dyer's polypore (Phaeolus schweinitzii), Henry Cowell State Park, California, 9/30/2022, Not Edible, Often used for dying whool
+//fungi2, Snowy inkcap (Coprinopsis nivea), Henry Cowell State Park, California, 9/30/2022, Not Edible, Commonly found growing from shit
+//fungi3, Golden pholiota (Pholiota aurivella), Sequoia National Park, California, 10/17/2022, Not Edible, Gastral upset upon consumption yet tastse like sugarless marshmallows
+//fungi4, Winter russula (Russula cremoricolor), Henry Cowell State Park, California, 1/15/2023, Edible, Fruits during the winter primarily throughout the west coast
+//fungi5, Witches' butter (Tremella mesenterica), Henry Cowell State Park, California, 1/15/2023, Edible, Chinese use it to add texture to soups
+//fungi6, ???, Henry Cowell State Park, California, 9/30/2022
+//fungi7, Violet crust (Punctularia atropurpurascens), Henry Cowell State Park, California, 1/15/2023, Not Edible, Parasitic tendencies
+//fungi8, Conifer tuft (Hypholoma capnoides), Nisene Marks State Park, California, 2/11/2023, Not Edible, Loves to grow on dead conifers
+//fungi9, Warlock's Butter (Exidia nigricans), Henry Cowell State Park, California, 1/15/2023, Edible, Found all throughout the year
+//fungi10, Lemon waxcap (Hygrocybe chlorophana), Henry Cowell State Park, 2/13/2023, Edible, Rare find in California
